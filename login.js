332819abcbc9dedcfa37c01590af0039e84f8a53
Aviweiam <nika.pomazova@gmail.com>
2025-04-19 11:50:17 -0400

    i think login works

--- login.js
+++ login.js
@@ -1,10 +1,4 @@
-import { Client, Account } from 'appwrite';
-
-//initialize the client
-const client = new Client().setEndpoint('https://fra.cloud.appwrite.io/v1').setProject('67fab4540027acd187eb');
-
-//initialize the account service
-const account = new Account(client);
+import {auth, signInWithEmailAndPassword} from "./firebaseInitializing.js";
 
 const loginButton = document.getElementById("loginButton");
 
@@ -13,29 +7,36 @@ const password = document.getElementById("password").value;
 
 loginButton.disabled = !(email && password);
 
-document.getElementById("loginBtn").addEventListener("click", async () => {
+document.getElementById("loginButton").addEventListener("click", async () => {
     const errorText = document.getElementById("errorDisplay").value;
 
     try {
-        //creating the session
-        await account.createEmailSession(email, password);
+        //logging the user in
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
     
-        //get user data
-        const user = await account.get();
-        console.log("Logged in:", user.name);
+        //checking if email is verified
+        if (!user.emailVerified) {
+          await auth.signOut(); //force logout if not verified
+          errorText.textContent ='Please verify your email first. Check your inbox.';
+        }
     
         //redirecting to main
-        window.location.href = "/Main.html";
-    
-    } catch (error) {
-
-        if (error.type === 'invalid_credentials') { //if the user does not exist
-            errorText.textContent = "Invalid email or password";
+        window.location.href = '/Main.html';
+      } catch (error) {
+        //handling specific errors
+        switch(error.code) {
+          case 'auth/user-not-found':
+            errorText.textContent = 'Email not registered. Sign up first.';
             if (confirm("Go to sign up?")) {
                 window.location.href = "/index.html";
             }
-        } else {
-            errorElement.textContent = "Login failed.";
+            break;
+          case 'auth/wrong-password':
+            errorText.textContent = 'Incorrect password.';
+            break;
+          default:
+            errorText.textContent = error.message;
         }
-    }
+      }
 });
