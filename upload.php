4620ed75809093e8211119ef5af31932495a9fb9
root <nika.pomazova@gmail.com>
2025-04-20 17:50:25 +0000

    from web

--- upload.php
+++ upload.php
@@ -1,5 +1,20 @@
 <?php
-$target_dir = "/var/www/html/uploads/";
+
+if ($_GET) {
+ // echo "We've got parameters \r\n";
+  $ufolder = $_GET['ufolder'];
+  $target_dir = "/var/www/html/uploads/{$ufolder}/";
+ // echo "Target dir is {$target_dir} \r\n";
+  } else {
+ // echo "No parameters passed";
+  $target_dir = "/var/www/html/uploads/";
+  }
+// Check if user directory exists
+if (!is_dir("{$target_dir}")) {
+   //echo "Dir does not exist - creating";
+   mkdir("{$target_dir}", 0777, true);
+}
+
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -42,6 +57,7 @@ if ($uploadOk == 0) {
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    header("Location: /Wardrobe.html");
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
