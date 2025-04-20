4da80e0f8f76fcf01d0bc10dc5271eb475ea34df
Aviweiam <nika.pomazova@gmail.com>
2025-04-20 12:05:59 -0400

    aa

--- firebaseInitializing.js
+++ firebaseInitializing.js
@@ -10,14 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API,
+    apiKey: "AIzaSyCQ907RTSmRyw38xBoE9kOK7af4Ut4wIG0",
     authDomain: "yourstylist.stereopi.com/",
     projectId: "yourstylist-450ba",
     storageBucket: "yourstylist-450ba.firebasestorage.app",
     messagingSenderId: "774264219494",
     appId: "1:774264219494:web:c207dfc2e053e87653caa7"
 };
-console.log(process.env.FIREBASE_API);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
